ef7cda14c447944e0fac9879b69ff75f3bc34935
Kenji_Takahashi <wozniakk@gmail.com>
2012-03-25 22:41:07 +0200

    lol

--- src/main/scala/engine/gates/io/binary.scala
+++ src/main/scala/engine/gates/io/binary.scala
@@ -3,8 +3,8 @@ package gates
 
 class BinaryInput(new_value : Boolean) extends IOGate {
     var value = new_value
-    def switch(new_value : Boolean) {
-        value = new_value
+    def switch() {
+        value = !value
     }
     override def compute : Boolean = value
 }
